689135590d0923369ae8faa064bda609b2cff495
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-01-16 01:23:24 +0530

    Base UI done

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 350,
+    height: 450,
+  });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
